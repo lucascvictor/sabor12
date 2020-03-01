f00215ba2e900e59fd3598511a5e78088691ba87
Lucas <lucas.c.victor@hotmail.com>
2020-03-01 01:49:25 -0300

    Login com facebook

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+  <title>Sabor do meio-dia</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="row">
+        <button class="btn col s3 l3 m3" onclick="FB.login();">LOGIN</button>
+      </div>
+    </div>
+  </body>
+  <script src="./js/facebook.js">
+     console.log(validarStatusFb());
+  </script>
+</html>
